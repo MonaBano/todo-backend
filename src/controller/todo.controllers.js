257a67cc7ba26dd8696813241235dcee9f32b2cd
MonaBano <bano.mona.a@gmail.com>
2024-01-12 02:41:03 +1100

    update model to store only the task

--- src/controller/todo.controllers.js
+++ src/controller/todo.controllers.js
@@ -32,25 +32,24 @@ export const updateTodo = async (req, res) => {
   const requestId = req.params.id;
   console.log(`requestId :: ${requestId}`);
   console.log(`body :: ${req.body}`);
-
+ 
   try {
     console.log(`Updating todo`);
-
-    const todo = await Todo.findOneAndUpdate(requestId, req.body);
+ 
+    const todo = await Todo.findByIdAndUpdate(requestId, req.body);
     res.status(200).send(todo);
   } catch (err) {
     res.status(400).send(err);
   }
-};
-
-export const deleteTodo = async (req, res) => {
-  const requestId = req.params.id;
+ };
+ 
+ export const deleteTodo = async (req, res) => {
   try {
     console.log(`Deleting Todo`, requestId);
-
-    const todo = await Todo.deleteOne({ id: requestId });
+ 
+    const todo = await Todo.findByIdAndRemove(requestId);
     res.status(200).send(todo);
   } catch (err) {
     res.status(400).send(err);
   }
-};
+ };
--- src/models/todo.models.js
+++ src/models/todo.models.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
 const TodoSchema = new mongoose.Schema({
-  id: String,
   task: String,
-  status: Boolean,
 });
 
 export const Todo = mongoose.model("Todo", TodoSchema);
